2c76256afc1cd1168c8f9f2f486ce516a42202e5
Mohamed Yusuf <momme40@hotmail.com>
2023-09-05 14:09:20 +0200

    klar med databas koppling

--- server.js
+++ server.js
@@ -2,11 +2,23 @@ const express = require ('express');
 const mongoose = require ('mongoose');
 const app = express();
 app.use(express.json());
+require('dotenv').config();
 
 
 app.get('/home', (req, res) => {
     res.json('home');
   });
+
+  mongoose.connect(
+    process.env.DB_URL,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+
+    },
+    console.log('connected to DB')
+
+  );
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
