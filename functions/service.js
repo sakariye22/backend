fa8469b6ce4b3cef583ffd64108adda26eb303d9
Mohamed Yusuf <momme40@hotmail.com>
2023-09-11 17:14:56 +0200

    ändring klar med firebase

---
+++ functions/service.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const app = require('./server.js');
+
+
+const PORT = process.env.PORT || 3000
+
+
+app.listen(PORT, () => {
+  console.log(`http server listening on port ${PORT}`)
+});
+
+
