3565e2acbaa449bdd339d2be5c01f4351633f3e6
zakariye <zakariye1942@icloud.com>
2023-09-16 08:44:26 +0200

    ready for fetch

--- functions/server.js
+++ functions/server.js
@@ -1,13 +1,49 @@
 const express = require('express');
-const app = express()
-
+const app = express();
+const cors = require('cors');
 
 app.use(express.json());
+app.use(cors());
 
+const Array2 = [
+  {
+    "id": 1,
+    "name": "John Doe",
+    "age": 28
+  },
+  {
+    "id": 2,
+    "name": "Jane Doe",
+    "age": 22
+  }
+];
 
-app.get('/', (req, res) => {
-  res.json({status: "ok"});
-});
+const getAll = async (req, res) => {
+  return { newMessage: "hej igen" };
+};
+
+const getAll2 = async (req, res) => {
+  return { newMessage2: "hallå" };
+};
+
+const getAll3 = async (req, res) => {
+  try {
+    const result1 = await getAll(req, res);
+    const result2 = await getAll2(req, res);
+
+    // Combine the results into a single object
+    const combinedResult = {
+      ...result1,
+      ...result2
+    };
+
+    res.json(combinedResult);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+app.get('/', getAll3);
 
 module.exports = app;
-
