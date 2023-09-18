729df75fe9249f9a2e8c0a93e6452872ae5cd1f0
zakariye <zakariye1942@icloud.com>
2023-09-18 08:54:45 +0200

    sending json instead of messages

--- functions/server.js
+++ functions/server.js
@@ -26,18 +26,31 @@ const getAll2 = async (req, res) => {
   return { newMessage2: "hallå" };
 };
 
-const getAll3 = async (req, res) => {
+/*const getAll3 = async (req, res) => {
   try {
     const result1 = await getAll(req, res);
     const result2 = await getAll2(req, res);
 
-    // Combine the results into a single object
+
     const combinedResult = {
       ...result1,
       ...result2
     };
 
     res.json(combinedResult);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+*/  const getAll3 = async (req, res) => {
+  try {
+    const result1 = { "firstName": "John" };
+    const result2 = { "lastName": "Doe" };
+
+    const jsonArray = [result1, result2];
+
+    res.json(jsonArray);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal Server Error' });
