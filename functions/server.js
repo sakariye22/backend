8c1a6f3a8e8aaeef305af2828449e56bafd07b9c
Mohamed Yusuf <momme40@hotmail.com>
2023-09-19 12:59:21 +0200

    ändringar klar

--- functions/server.js
+++ functions/server.js
@@ -5,47 +5,9 @@ const cors = require('cors');
 app.use(express.json());
 app.use(cors());
 
-const Array2 = [
-  {
-    "id": 1,
-    "name": "John Doe",
-    "age": 28
-  },
-  {
-    "id": 2,
-    "name": "Jane Doe",
-    "age": 22
-  }
-];
-
-const getAll = async (req, res) => {
-  return { newMessage: "hej igen" };
-};
-
-const getAll2 = async (req, res) => {
-  return { newMessage2: "hallå" };
-};
-
-/*const getAll3 = async (req, res) => {
+ const getAll3 = async (req, res) => {
   try {
-    const result1 = await getAll(req, res);
-    const result2 = await getAll2(req, res);
-
-
-    const combinedResult = {
-      ...result1,
-      ...result2
-    };
-
-    res.json(combinedResult);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-};
-*/  const getAll3 = async (req, res) => {
-  try {
-    const result1 = { "firstName": "John" };
+    const result1 = { "firstName": "jack" };
     const result2 = { "lastName": "Doe" };
 
     const jsonArray = [result1, result2];
