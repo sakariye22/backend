dcb8cf8506c1fbf789b4a998a0d1fdf175eb1952
Jonathan <jonathan.jallow@hotmail.com>
2023-09-20 11:57:08 +0200

    getall

--- functions/server.js
+++ functions/server.js
@@ -5,6 +5,35 @@ const cors = require('cors');
 app.use(express.json());
 app.use(cors());
 
+
+const data = [
+  {
+    "id": 0,
+    "firstName": "Daenerys",
+    "lastName": "Targaryen",
+    "fullName": "Daenerys Targaryen",
+    "title": "Mother of Dragons",
+    "family": "House Targaryen",
+    "image": "daenerys.jpg",
+    "imageUrl": "https://thronesapi.com/assets/images/daenerys.jpg"
+  },
+  {
+    "id": 1,
+    "firstName": "Samwell",
+    "lastName": "Tarly",
+    "fullName": "Samwell Tarly",
+    "title": "Maester",
+    "family": "House Tarly",
+    "image": "sam.jpg",
+    "imageUrl": "https://thronesapi.com/assets/images/sam.jpg"
+  }
+];
+
+
+app.get('/data', (req, res) => {
+  res.json(data);
+});
+
 app.get('/data/:id', (req, res) => {
   const id = parseInt(req.params.id);
   const entry = data.find(item => item.id === id);
